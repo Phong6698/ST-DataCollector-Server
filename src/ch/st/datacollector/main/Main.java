9134f111978d0f0c60969814fd9a4d9b9e84811a
Phong6698 <Phong@hotmail.ch>
2016-05-15 00:54:11 +0200

    Hello

--- src/ch/st/datacollector/main/Main.java
+++ src/ch/st/datacollector/main/Main.java
@@ -3,7 +3,11 @@ package ch.st.datacollector.main;
 public class Main {
 
 	public static void main(String[] args) {
-	    	System.out.println("Hello");
+        System.out.println("Hello");
+
+        for(int i = 0; i <= 10; i++){
+            System.out.println("Hello "+i);
+        }
 
 	}
 
